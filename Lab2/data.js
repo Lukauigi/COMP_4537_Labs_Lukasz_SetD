c6a6a4df9ab73cae18545e1dea6483b97ca4e579
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2022-09-16 18:33:40 -0700

    Tested user-defined modules

---
+++ Lab2/data.js
@@ -0,0 +1,6 @@
+const name_ = "John";
+const age = 21;
+
+module.exports = { name_, age};
+//export vars, funcs, etc to module for logic in separate files to use
+console.log(module.exports); 
---
+++ Lab2/test.js
@@ -0,0 +1,8 @@
+const { age, name_ } = require('./data.js'); //import from module (file, dependency, etc)
+//use brackets to use mutliple things from module. also called destructuring
+console.log(x);
+
+printNameAndAge = () => {
+    console.log(name_, age);
+}
+printNameAndAge
