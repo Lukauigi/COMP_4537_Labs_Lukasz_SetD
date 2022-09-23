e4d3a42079743c7ac6601b944c9926e83858f3b0
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2022-09-23 11:41:44 -0700

    Create test requests for routes

--- lab3/server.js
+++ lab3/server.js
@@ -3,28 +3,50 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+var { unicornsJSON } = require('./data.js')
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
   }
 )
 
 app.get('/api/v1/unicorns', (req, res) => {
-    res.send('All the unicorns')
+    res.json(unicornsJSON)
   })
   
   app.post('/api/v1/unicorn', (req, res) => {
+    unicornsJSON.push(req.body)
+    console.log(unicornsJSON)
     res.send('Create a new unicorn')
   })
   
   app.get('/api/v1/unicorn/:id', (req, res) => {
-    res.send('Get a unicorn')
+    var foundUnicorn = false
+    for (i = 0; i < unicornsJSON.length; i++) {
+        if (unicornsJSON[i]._id == req.params.id) {
+            foundUnicorn = true
+            break
+        }
+    }
+
+    if (foundUnicorn) { res.json(unicornsJSON[i]); return }
+    res.json({ msg: "not found" })
   })
   
   app.patch('/api/v1/unicorn/:id', (req, res) => {
-    res.send('Update a unicorn')
+    unicornsJSON = unicornsJSON.map(({ _id, ...aUnicorn }) => {
+        if (_id == req.body._id) {
+          console.log("Bingo!");
+          return req.body
+        } else
+          return aUnicorn
+      })
+      res.send("Updated successfully!")
   })
   
   app.delete('/api/v1/unicorn/:id', (req, res) => {
-    res.send('Delete a unicorn')
+    unicornsJSON = unicornsJSON.filter((element) => element._id != req.params.id)
+    console.log(unicornsJSON)
+    res.send("Deleted successfully?")
   })
   
