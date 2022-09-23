3bdf110178acbffa1e4b8a506272e579d08a6841
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2022-09-23 12:05:06 -0700

    Fix json response issue

--- lab3/server.js
+++ lab3/server.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 5000
 
 var { unicornsJSON } = require('./data.js')
+//var unicornsJSON = []
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
@@ -12,18 +13,22 @@ app.listen(port, () => {
 
 app.get('/api/v1/unicorns', (req, res) => {
     res.json(unicornsJSON)
-  })
+})
+
+app.use(express.json())
   
-  app.post('/api/v1/unicorn', (req, res) => {
+app.post('/api/v1/unicorn', (req, res) => {
     unicornsJSON.push(req.body)
     console.log(unicornsJSON)
     res.send('Create a new unicorn')
-  })
-  
-  app.get('/api/v1/unicorn/:id', (req, res) => {
+})
+
+app.get('/api/v1/unicorn/:id', (req, res) => {
+    console.log('find a unicorn')
     var foundUnicorn = false
     for (i = 0; i < unicornsJSON.length; i++) {
         if (unicornsJSON[i]._id == req.params.id) {
+            console.log(unicornsJSON[i])
             foundUnicorn = true
             break
         }
@@ -31,22 +36,21 @@ app.get('/api/v1/unicorns', (req, res) => {
 
     if (foundUnicorn) { res.json(unicornsJSON[i]); return }
     res.json({ msg: "not found" })
-  })
-  
-  app.patch('/api/v1/unicorn/:id', (req, res) => {
+})
+
+app.patch('/api/v1/unicorn/:id', (req, res) => {
     unicornsJSON = unicornsJSON.map(({ _id, ...aUnicorn }) => {
         if (_id == req.body._id) {
-          console.log("Bingo!");
-          return req.body
+            console.log("Bingo!");
+            return req.body
         } else
-          return aUnicorn
-      })
-      res.send("Updated successfully!")
-  })
-  
-  app.delete('/api/v1/unicorn/:id', (req, res) => {
+            return aUnicorn
+        })
+    res.send("Updated successfully!")
+})
+
+app.delete('/api/v1/unicorn/:id', (req, res) => {
     unicornsJSON = unicornsJSON.filter((element) => element._id != req.params.id)
     console.log(unicornsJSON)
     res.send("Deleted successfully?")
-  })
-  
+})
