f7f96da38068020fca3bb4543eb9ee9101f2d499
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2022-09-23 15:43:22 -0700

    Add new routing logic for replicating local server

--- lab3/server.js
+++ lab3/server.js
@@ -3,13 +3,29 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-var { unicornsJSON } = require('./data.js')
-//var unicornsJSON = []
+//var { unicornsJSON } = require('./data.js')
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-  }
-)
+const { writeFile, readFile } = require('fs')
+const util = require('util')
+const writeFileAsync = util.promisify(writeFile)
+const readFileAsync = util.promisify(readFile)
+var unicornsJSON = []
+
+app.listen(port, async () => {
+    try {
+      unicornsJSON = await readFileAsync('./data.json', 'utf-8')
+      if (!unicornsJSON) {
+        console.log("Could not read the file");
+        return
+      }
+      unicornsJSON = JSON.parse(unicornsJSON)
+      console.log(unicornsJSON);
+    } catch (error) {
+      console.log(error);
+    }
+  
+    console.log(`Example app listening on port ${port}`)
+})
 
 app.get('/api/v1/unicorns', (req, res) => {
     res.json(unicornsJSON)
@@ -18,9 +34,14 @@ app.get('/api/v1/unicorns', (req, res) => {
 app.use(express.json())
   
 app.post('/api/v1/unicorn', (req, res) => {
-    unicornsJSON.push(req.body)
-    console.log(unicornsJSON)
-    res.send('Create a new unicorn')
+    // unicornsJSON.push(req.body)
+    // console.log(unicornsJSON)
+    // res.send('Create a new unicorn')
+
+    //update the file
+    writeFileAsync('./data.js', JSON.stringify(unicornsJSON), 'utf-8')
+        .then(() => { })
+        .catch((err) => { console.log(err); })
 })
 
 app.get('/api/v1/unicorn/:id', (req, res) => {
@@ -39,18 +60,28 @@ app.get('/api/v1/unicorn/:id', (req, res) => {
 })
 
 app.patch('/api/v1/unicorn/:id', (req, res) => {
-    unicornsJSON = unicornsJSON.map(({ _id, ...aUnicorn }) => {
-        if (_id == req.body._id) {
-            console.log("Bingo!");
-            return req.body
-        } else
-            return aUnicorn
-        })
-    res.send("Updated successfully!")
+    // unicornsJSON = unicornsJSON.map(({ _id, ...aUnicorn }) => {
+    //     if (_id == req.body._id) {
+    //         console.log("Bingo!");
+    //         return req.body
+    //     } else
+    //         return aUnicorn
+    //     })
+    // res.send("Updated successfully!")
+
+    //update the file
+    writeFileAsync('./data.js', JSON.stringify(unicornsJSON), 'utf-8')
+        .then(() => { })
+        .catch((err) => { console.log(err); })
 })
 
 app.delete('/api/v1/unicorn/:id', (req, res) => {
-    unicornsJSON = unicornsJSON.filter((element) => element._id != req.params.id)
-    console.log(unicornsJSON)
-    res.send("Deleted successfully?")
+    // unicornsJSON = unicornsJSON.filter((element) => element._id != req.params.id)
+    // console.log(unicornsJSON)
+    // res.send("Deleted successfully?")
+
+    //update the file
+    writeFileAsync('./data.js', JSON.stringify(unicornsJSON), 'utf-8')
+        .then(() => { })
+        .catch((err) => { console.log(err); })
 })
