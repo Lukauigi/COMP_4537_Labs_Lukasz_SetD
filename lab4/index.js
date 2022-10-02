4db7803207ecc2cd007f15021c7534dc48f1afba
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2022-10-02 13:42:21 -0700

    Initiate project, install technologies, and add route stubs

---
+++ lab4/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+const port = 5000
+
+app.listen(port, async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/test')
+  } catch (error) {
+    console.log('db error');
+  }
+  console.log(`Example app listening on port ${port}`)
+}
+)
+
+// app.get('/api/v2/unicorns')           // - get all the unicorns
+// app.post('/api/v2/unicorn')          // - create a new unicorn
+// app.get('/api/v2/unicorn/:id')       // - get a unicorn
+// app.patch('/api/v2/unicorn/:id')     // - update a unicorn
+// app.delete('/api/v2/unicorn/:id')       // - delete unicorn
