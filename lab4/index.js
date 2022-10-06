b63014023849ecb96d2563d59e976a00f9e123b4
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2022-10-05 19:59:57 -0700

    Add logic to routes and unicorn schema

--- lab4/index.js
+++ lab4/index.js
@@ -14,22 +14,73 @@ app.listen(port, async () => {
 }
 )
 
+
+const { Schema } = mongoose;
+
+const unicornSchema = new Schema({
+  "name": String, // String is shorthand for {type: String}
+  "weight": Number,
+  "loves": [String],
+  "gender": {
+    enum: ["f", "m"]
+  },
+  "vampires": Number,
+  "dob": Date
+});
+
+const unicornModel = mongoose.model('unicorns', unicornSchema); // unicorns is the name of the collection in db
+
+app.use(express.json())
+
 app.get('/api/v2/unicorns', (req, res) => {
-  res.send('All the unicorns')
+  unicornModel.find({})
+    .then(docs => {
+      console.log(docs)
+      res.json(docs)
+    })
+    .catch(err => {
+      console.error(err)
+      res.json({ msg: "db reading .. err.  Check with server devs" })
+    })
 })
+
 
 app.post('/api/v2/unicorn', (req, res) => {
-  res.send('Create a new unicorn')
-})
+  unicornModel.create(req.body, function (err) {
+    if (err) console.log(err);
+    // saved!
+  });
+  res.json(req.body)
+})  
 
 app.get('/api/v2/unicorn/:id', (req, res) => {
-  res.send('Get a unicorn')
+  console.log(req.params.id);
+  unicornModel.find({ _id: mongoose.Types.ObjectId(`${req.params.id}`) })
+    .then(doc => {
+      console.log(doc)
+      res.json(doc)
+    })
+    .catch(err => {
+      console.error(err)
+      res.json({ msg: "db reading .. err.  Check with server devs" })
+    })
 })
 
 app.patch('/api/v2/unicorn/:id', (req, res) => {
-  res.send('Update a unicorn')
+  const { _id, ...rest } = req.body;
+  unicornModel.updateOne({ _id: mongoose.Types.ObjectId(req.params.id) }, rest, function (err, res) {
+    if (err) console.log(err)
+    console.log(res)
+  });
+
+  res.send("Updated successfully!")
 })
 
 app.delete('/api/v2/unicorn/:id', (req, res) => {
-  res.send('Delete a unicorn')
+  unicornModel.deleteOne({ _id: mongoose.Types.ObjectId(req.params.id) }, function (err, result) {
+    if (err) console.log(err);
+    console.log(result);
+  });
+
+  res.send("Deleted successfully?")
 })
