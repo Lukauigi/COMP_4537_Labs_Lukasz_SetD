f1047de0a7293b3b44e0f892cd90c0e5392d6377
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2022-10-05 19:27:07 -0700

    Added simple responses

--- lab4/index.js
+++ lab4/index.js
@@ -14,8 +14,22 @@ app.listen(port, async () => {
 }
 )
 
-// app.get('/api/v2/unicorns')           // - get all the unicorns
-// app.post('/api/v2/unicorn')          // - create a new unicorn
-// app.get('/api/v2/unicorn/:id')       // - get a unicorn
-// app.patch('/api/v2/unicorn/:id')     // - update a unicorn
-// app.delete('/api/v2/unicorn/:id')       // - delete unicorn
+app.get('/api/v2/unicorns', (req, res) => {
+  res.send('All the unicorns')
+})
+
+app.post('/api/v2/unicorn', (req, res) => {
+  res.send('Create a new unicorn')
+})
+
+app.get('/api/v2/unicorn/:id', (req, res) => {
+  res.send('Get a unicorn')
+})
+
+app.patch('/api/v2/unicorn/:id', (req, res) => {
+  res.send('Update a unicorn')
+})
+
+app.delete('/api/v2/unicorn/:id', (req, res) => {
+  res.send('Delete a unicorn')
+})
