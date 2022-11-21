d2f110a059f26d4961e2fb0a5cc81567af860858
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2022-11-21 01:25:30 -0800

    Implement multiple pages and pagination

--- Labs/lab10/src/App.js
+++ Labs/lab10/src/App.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 import Page from './Page'
+import Pagination from './Pagination'
 
 function App() {
   const [pokemons, setPokemons] = useState([])
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pokemonsPerPage] = useState(10);
 
   useEffect(() => {
     axios.get('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json')
@@ -16,8 +18,20 @@ function App() {
       .catch(err => console.log("err", err))
   }, [])
 
+  const indexOfLastRecord = currentPage * pokemonsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - pokemonsPerPage;
+  const currentPokemons = pokemons.slice(indexOfFirstRecord, indexOfLastRecord)
+  const numberOfPages = Math.ceil(pokemons.length / pokemonsPerPage);
+
   return (
-    <Page currentPokemons={pokemons} />
+    <>
+      < Page currentPokemons={currentPokemons} currentPage={currentPage} />
+      < Pagination
+        numberOfPages={numberOfPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </>
   );
 }
 
--- Labs/lab10/src/Page.js
+++ Labs/lab10/src/Page.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Pokemon from './Pokemon'
 
-function page({ currentPokemons }) {
+function page({ currentPokemons, currentPage }) {
   return (
     <div>
+      <h1>
+        Page number {currentPage}
+      </h1>
       <div className="pokemon-list">
         {
           currentPokemons.map(item => {
